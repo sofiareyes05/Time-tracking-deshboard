1c13fa18e7d0e06c3d56a9f4b61120750713fa75
sofiareyes05 <reyesamayasofia@gmail.com>
2026-02-19 11:25:40 -0500

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export const App = () => {
       {/* Tarjeta de Perfil (Jeremy) */}
       <div className="profile-card">
         <div className="profile-top">
-          <img src="./jeremy.png" alt="Jeremy" className="avatar" />
+          <img src="./public/images.png" alt="Jeremy" className="avatar" />
           <div className="profile-text">
             <p>Report for</p>
             <h1>Jeremy Robson</h1>
@@ -22,11 +22,11 @@ export const App = () => {
 
       {/* Bloque de Actividades */}
       <User nombre="Work" horas="32hrs" anterior="36hrs" color="work" />
-      <User nombre="Play" horas="15hrs" anterior="8hrs" color="play" />
-      <User nombre="Study" horas="49hrs" anterior="7hrs" color="study" />
-      <User nombre="Exercise" horas="46hrs" anterior="5hrs" color="exercise" />
-      <User nombre="Social" horas="57hrs" anterior="10hrs" color="social" />
-      <User nombre="Self Care" horas="25hrs" anterior="2hrs" color="self-care" />
+      <User nombre="Play" horas="10hrs" anterior="8hrs" color="play" />
+      <User nombre="Study" horas="4hrs" anterior="7hrs" color="study" />
+      <User nombre="Exercise" horas="4hrs" anterior="5hrs" color="exercise" />
+      <User nombre="Social" horas="5hrs" anterior="10hrs" color="social" />
+      <User nombre="Self Care" horas="2hrs" anterior="2hrs" color="self-care" />
     </div>
   )
 }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,9 @@
 export const User = ({ nombre, horas, anterior, color }) => {
   return (
     <div className={`card-container ${color}`}>
-      {/* Este div servirá para mostrar el icono decorativo de fondo */}
-      <div className="icon-background"></div>
-      
       <div className="card-content">
         <div className="card-header">
-          <span className="title">{nombre}</span>
+          <p className="title">{nombre}</p>
           <button className="dots-btn">...</button>
         </div>
         <div className="card-body">
